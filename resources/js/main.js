3cad55188fb156f025c4ca3cdd6bb69835905f1a
Chip-L <chip_long@yahoo.com>
2017-12-22 22:56:10 -0700

    header hides - done (carousel images are different sizes?)

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,5 +1,30 @@
 $(document).ready( function () {
-  $('.carousel').slick({
+  var $carousel = $('.carousel');
+  var $topNavbar = $('#topNavbar');
+  var scrollDist = 0;
+
+  initCarousel($carousel);
+  $(window).scroll(function () {
+    var $scrollTop = $(this).scrollTop();
+
+    if(($scrollTop - scrollDist) > 50) {
+      var $navbarHeight = $topNavbar.css('height');
+      $topNavbar.animate({
+        top: "-" + $navbarHeight
+      }, 150);
+      scrollDist = $scrollTop;
+    } else if ((scrollDist - $scrollTop) > 50) {
+      $topNavbar.animate({
+        top: 0
+      }, 150);
+      scrollDist = $scrollTop;      
+    };
+
+  });
+});
+
+function initCarousel($element) {
+  $element.slick({
     slidesToScroll: 1,
     autoplay: true,
     speed: 4000,
@@ -7,5 +32,5 @@ $(document).ready( function () {
     draggable: false,
     pauseOnFocus: false,
     pauseOnHover: false,
-  })
-});
+  });
+}
